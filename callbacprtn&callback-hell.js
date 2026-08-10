40d6f697051b836f5f638df9f2ca3f0f6d36ac69
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-08-11 01:39:06 +0530

    Added new files

--- callbacprtn&callback-hell.js
+++ callbacprtn&callback-hell.js
@@ -1,2 +1,12 @@
 //==========================CALLBACK--FUNCTION=======================================
 // ek function ko aap ek aur fucntion bhejo  uskay parameter mai , to wo  parameter wala function kahlata hai call back function
+function profleOuter(username, callback)
+{setTimeout(()=>{
+   
+    callback({username , age:22 , email:"gauri@gamai.com"})
+},2000)
+}
+profleOuter("gaurav",function (data) {
+    
+
+})
