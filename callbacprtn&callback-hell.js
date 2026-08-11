445b9533530623c1ac9dc66103113d6dffc773d7
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-08-11 13:52:56 +0530

    Added new files

--- callbacprtn&callback-hell.js
+++ callbacprtn&callback-hell.js
@@ -3,10 +3,31 @@
 function profleOuter(username, callback)
 {setTimeout(()=>{
    
-    callback({username , age:22 , email:"gauri@gamai.com"})
+    callback(
+        { _id: 1234, 
+            username ,
+             age:22 ,
+              email:"gauri@gamai.com"}
+            )
 },2000)
-}
-profleOuter("gaurav",function (data) {
-    
 
-})
+};
+function sarePostlekarAaao(id,callback) {
+    setTimeout(() =>{
+        callback({
+            _id: id,
+            post:["hey heloo"]
+        })
+
+    },3000)
+    
+}
+
+
+profleOuter("gaurav", function (data) {
+    console.log(data)
+  sarePostlekarAaao(data._id, function(posts) {
+    console.log(posts)
+
+  });
+});
